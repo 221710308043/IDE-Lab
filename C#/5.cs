5cce57eabcb57d728378cae37f40d52c3815e868
Hari Naga Sai Perisetla <70635503+221710308043@users.noreply.github.com>
2020-11-12 15:10:46 +0530

    first commit

--- C#/5.cs
+++ C#/5.cs
@@ -8,7 +8,7 @@ namespace myApp
     {
         static void Main()
         {
-            var name = "Pavan";
+            var name = "Hari";
             Console.WriteLine($"Hello {name.ToUpper()}!");
         }
     }
